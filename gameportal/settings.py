782610a5112f6bdbb5b4099292017c7e9ea9a926
Piyal Hossein <piyal13133@gmail.com>
2024-02-13 13:48:11 +0600

    initial commit

--- gameportal/settings.py
+++ gameportal/settings.py
@@ -85,12 +85,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
